1009e9ec8de9a4938170792a667d52f7b3490ede
jared lutz and Matt Nardoni <matt@>
2018-05-08 16:34:34 -0700

    add buttons to update or remove entries from database

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -11,13 +11,18 @@ import { FirebaseListObservable } from 'angularfire2/database';
   providers: [AlbumService]
 })
 export class MarketplaceComponent implements OnInit{
-  constructor(private router: Router, private albumService: AlbumService) {}
+  albums: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
+
+  constructor(
+    private router: Router,
+    private albumService: AlbumService
+  ) {}
 
   ngOnInit(){
     this.albums = this.albumService.getAlbums();
   }
 
-  albums: FirebaseListObservable<any[]>;
  goToDetailPage(clickedAlbum) {
    this.router.navigate(['albums', clickedAlbum.$key]);
  };
